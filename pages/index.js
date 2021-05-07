7bdb71e51473f79d81fc77d8cf395037a000c143
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-07 12:30:04 -0400

    added Head in HomePage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Home</h1>
-      <Link href="/about">About</Link>
+      <Head>
+        <title>DJ Events</title>
+      </Head>
+      {/* <Link href="/about">About</Link> */}
       {/* we don't want <a></a> because we want everything happens on client side
       <a href='/about'>About</a> */}
     </div>
