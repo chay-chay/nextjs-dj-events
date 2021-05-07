2b2ca1939d332139e26d8d6ed7f5d49a121a4b4d
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-07 15:29:45 -0400

    10. Header & Footer

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,8 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <>
-    <h1>Hello</h1> {/* this is going to show every pages */}
-    <Component {...pageProps} />
+   {/*  <h1>Hello</h1>  this is going to show every pages */}
+    <Component {...pageProps} /> 
     </>
 }
 
