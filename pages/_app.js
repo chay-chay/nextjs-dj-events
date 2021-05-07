a607d3b686d2450cfdf1773fd9ef85bc38b766e4
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-06 21:56:07 -0400

    6. Creating a Next.js App

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <h1>Hello</h1> {/* this is going to show every pages */}
+    <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
