2228d2fe741a4d211d1ee5b297edf58986482c7a
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-07 12:51:09 -0400

    added Layout

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
-import Link from "next/link";
-import Head from "next/head";
+// import Link from "next/link";
+// import Head from "next/head";
+import Layout from '../components/Layout'
 
 export default function HomePage() {
   return (
-    <div>
-      <Head>
+    <Layout>
+      <h1>Home</h1>
+      {/* <Head>
         <title>DJ Events</title>
-      </Head>
+        <meta name='description' content='Welcome to DJ Events' />
+      </Head> */}
+
       {/* <Link href="/about">About</Link> */}
       {/* we don't want <a></a> because we want everything happens on client side
       <a href='/about'>About</a> */}
-    </div>
+    </Layout>
   );
 }
