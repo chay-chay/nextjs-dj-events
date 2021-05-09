253695f0f63df01bcca7a89de073f2f9c262ec46
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-08 23:31:42 -0400

    added styles and text of 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,16 @@
+import Link from 'next/link'
 import Layout from '../components/layout'
 import styles from '../styles/404.module.css';
 
 export default function NotFoundPage() {
     return (
-        <div>
+        <Layout title='Page Not Found'>
+            <div className={styles.error}>
+                <h1>404</h1>
+                <h4>Sorry, there is nothing here</h4>
+                <Link href='/'>Go Back Home</Link>
+            </div>
             
-        </div>
+        </Layout>
     )
 }
