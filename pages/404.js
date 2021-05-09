e386092ba04c9e623a8ddf68780e3a9d10c7bab9
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-08 23:21:03 -0400

    added 404 page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Layout from '../components/layout'
+import styles from '../styles/404.module.css';
+
+export default function NotFoundPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
