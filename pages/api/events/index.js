0eda8f57c7046a1ca39482e6ce146a33f30cdf79
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-19 15:43:52 -0400

    customs for 'GET'

--- pages/api/events/index.js
+++ pages/api/events/index.js
@@ -1,4 +1,10 @@
 const {events} = require('./data.json')
 export default (req, res) => {
-  res.status(200).json( events )
+  if (req.method === 'GET') {
+    res.status(200).json( events )
+  } else {
+    res.setHeader('Allow', ['GET'])
+    res.status(405).json({ message: `Medthod ${req.method} is not allowed` })
+  }
+  
 }
