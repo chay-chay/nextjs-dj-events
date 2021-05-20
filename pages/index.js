c5cd052a63b0db2ded0c7e66afe933c9afdc67b6
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-19 23:48:40 -0400

    added images to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 // import Link from "next/link";
 // import Head from "next/head";
-import Layout from '@/components/Layout'
-import {API_URL} from '@/config/index'
+import Layout from "@/components/Layout";
+import EventItem from '@/components/EventItem'
+import { API_URL } from "@/config/index";
 
-export default function HomePage(events) {
-  console.log(events) 
+export default function HomePage({events}) {
+  console.log(events);
+  // console.log(Array.Isarray(events))
+
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+      
       {/* <Head>
         <title>DJ Events</title>
         <meta name='description' content='Welcome to DJ Events' />
@@ -21,14 +30,14 @@ export default function HomePage(events) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events`)
-  const events = await res.json()
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
 
   // console.log(events)
   // if we do console.log here, it's going to show data in terminal here not on client side (localhost)
-  
+
   return {
     props: { events },
-    revalidate: 1 // if it doesn't find it, it will make the request again to find it 1 second delay when the data has changed
-  }
+    revalidate: 1, // if it doesn't find it, it will make the request again to find it 1 second delay when the data has changed
+  };
 }
