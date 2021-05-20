68a17e98073d169ef376a6b2cc5e8d8f391e6229
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-19 22:39:04 -0400

    added index.js config

---
+++ config/index.js
@@ -0,0 +1,3 @@
+export const API_URL =
+    process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
+    // it means if doesn't find NEXT_PUBLIC variable, it's going to show localhost
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 // import Link from "next/link";
 // import Head from "next/head";
 import Layout from '@/components/Layout'
+import {API_URL} from '@/config/index'
 
 export default function HomePage() {
   return (
@@ -16,4 +17,16 @@ export default function HomePage() {
       <a href='/about'>About</a> */}
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  // console.log(events)
+  // if we do console.log here, it's going to show data in terminal here not on client side (localhost)
+  
+  return {
+    props: {}
+  }
 }
